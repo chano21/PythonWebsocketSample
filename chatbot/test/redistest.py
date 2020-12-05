ec34a67bff0cf71b1bda67a41e219c18bd5d1b23
chano21 <chano22@naver.com>
2020-12-05 18:11:04 +0900

    test(redisclient) 레디스 pubsub 테스트 완료

--- chatbot/test/redistest.py
+++ chatbot/test/redistest.py
@@ -1,7 +1,28 @@
 import unittest
+import configparser
+import redis
+import time
+redisconfig='REDIS_SERVER'
+filedir = '../configs/redis_config.ini'
 
-class RedisClientTest(unittest.TestCase):
-    pass
+class RedisPubSubTest(unittest.TestCase):
+  
+    def testRedisClient(self):
+        config = configparser.ConfigParser()
+        config.read(filedir)
+        password = config[redisconfig]['REQUIREPASS']
+        host = config[redisconfig]['HOST']
+        port = config[redisconfig]['PORT']
+        r = redis.StrictRedis(host=host,port=port, db=0, encoding='utf-8',decode_responses=True)
+        ps = r.pubsub()
+        ps.subscribe('room1')
+        r.publish('room1',"hello world")
+        ps.get_message()
+        messages=ps.get_message()
+        ps.get_message()
+        data = messages['data'];
+        print(data) 
+        print(data) 
 
 if __name__ == '__main__':
     unittest.main()
