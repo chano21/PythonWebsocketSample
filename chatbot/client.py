d7e3f7fbfa7de53164c81db49240ec137f67165f
chano21 <chano22@naver.com>
2020-12-07 21:17:21 +0900

    fix(client) 웹소켓 클라이언트 수정중

--- chatbot/client.py
+++ chatbot/client.py
@@ -1,135 +1,42 @@
-import gevent
-from gevent import monkey
-monkey.patch_all()
+import websocket
+import json
+try:
+    import thread
+except ImportError:
+    import _thread as thread
+import time
 
-from websocket import create_connection
-ws = create_connection("ws://echo.websocket.org")
+def on_message(ws, message):
+    print(message)
 
-pool = gevent.pool.Pool(1000)
+def on_error(ws, error):
+    print(error)
 
-publishs=[]    
+def on_close(ws):
+    print("### closed ###")
 
-class RedisConnector():
+def on_open(ws):
+    def run(*args):
+        for i in range(3):
+            time.sleep(1)
+            text ='{ "name":"John", "age":"30", "city":"New York"}'
+     #       ws.send("Hello %d" % i)
+            ws.send(str(text))
+     #       ws.send(json.loads(text))
     
-    def __init__(self):
-        self.sockets = {}
-        self.redisconfig='REDIS_SERVER'
-        self.filedir='../configs/redis_config.ini'
-    def redisConnection(self):
-        config = configparser.ConfigParser()
-        config.read(self.filedir)
-        password = config[self.redisconfig]['REQUIREPASS']
-        host = config[self.redisconfig]['HOST']
-        port = config[self.redisconfig]['PORT']
-        r = redis.StrictRedis(host=host,port=port, db=0, encoding='utf-8',decode_responses=True)
-        self.pubsub=r.pubsub()
-        self.rc=r
-
-#redistool = RedisConnector()
-
-class GeventPool():
-    pass
-
-class MemoryBroker():
-    def __init__(self):
-        self.sockets = {}
-
-    def subscribe(self, key, socket):
-        if key not in self.sockets:
-            self.sockets[key] = set()
-        
-        if socket in self.sockets[key]:
-            return
-
-        self.sockets[key].add(socket)
-    
-    def publish(self, key, data):
-        for socket in self.sockets[key]:
- #           redistool.rc.publish(key,data)
- #           messages=redistool.pubsub.get_message()
- #           data = messages['data'];
-            socket.on_broadcast(data)
-            publishs.append(pool.spawn(socket.on_broadcast(data)))
-        gevent.joinall(publishs)
-  
-            
-        
-    def unsubscribe(self, key, socket):
-        if key not in self.sockets: return
-
-        self.sockets[key].remove(socket)
+        time.sleep(1)
+        #ws.close()
+        print("thread terminating...")
+    thread.start_new_thread(run, ())
 
 
-# class MemoryBroker():
-#     def __init__(self):
-#         self.sockets = {}
-
-#     def subscribe(self, key, socket):
-#         if key not in self.sockets:
-#             self.sockets[key] = set()
-
-#         if socket in self.sockets[key]:
-#             return
-
-#         self.sockets[key].add(socket)
-
-#     def publish(self, key, data):
-#         for socket in self.sockets[key]:
-#             socket.on_broadcast(data)
-
-#     def unsubscribe(self, key, socket):
-#         if key not in self.sockets: return
-
-#         self.sockets[key].remove(socket)
-
-
-broker = MemoryBroker()
-
-
-class Chat(WebSocketApplication):
-    
-    def on_open(self, *args, **kwargs):
-        start = time.time()
-        self.userid = uuid.uuid4()
-        broker.subscribe('room1', self)
-  #      redistool.pubsub.subscribe(self.roomkey)
-        print('on_open ' +str(time.time() - start))
-
-    def on_close(self, *args, **kwargs):
-        start = time.time()
-        broker.unsubscribe('room1', self)
-        print('on_close ' +str(time.time() - start))
-
-    def on_message(self, message, *args, **kwargs):
-        start = time.time()
-        if not message: return
-
-        data = json.loads(message)
-        data['user'] = self.userid.hex
-        
-        broker.publish('room1', data)
-        print('on_message ' +str(time.time() - start))
-        
-    def on_broadcast(self, data):
-        start = time.time()
-        message=json.dumps(data)
-        print('dump before ' +str(time.time() - start))
-        start = time.time()
-        self.ws.send(message)
-        print('dump after ' +str(time.time() - start))
-       
-
-def index(environ, start_response):
-    start_response('200 OK', [('Content-type','text/html')])
-    htmgl = open('index.html', 'rb').read()
-    return [htmgl]
-
-
-application = Resource([
-    ('^/chat', Chat),
-    ('^/', index)
-])
-
-
-if __name__ == '__main__':
-    WSGIServer('{}:{}'.format('0.0.0.0', 8000), application, handler_class=WebSocketHandler).serve_forever()
+if __name__ == "__main__":
+    websocket.enableTrace(True)
+    clientsize = 5
+    for i in range(clientsize):
+        ws = websocket.WebSocketApp("ws://localhost:8000/chat",
+                              on_message = on_message,
+                              on_error = on_error,
+                              on_close = on_close)
+        ws.on_open = on_open
+        ws.run_forever()
