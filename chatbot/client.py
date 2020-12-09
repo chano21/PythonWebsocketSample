7acfb4a875c1945ef69e63ab15ab4598c7c90d51
chano21 <chano22@naver.com>
2020-12-09 14:34:18 +0900

    finish(client) 채팅서버 클라이언트 테스트 완료

--- chatbot/client.py
+++ chatbot/client.py
@@ -1,42 +1,49 @@
 import websocket
-import json
-try:
-    import thread
-except ImportError:
-    import _thread as thread
+from multiprocessing import Process
 import time
+import os
+import gevent.pool
 
+pool=gevent.pool.Pool(10)
 def on_message(ws, message):
-    print(message)
-
+     print("Received '%s'" % message)
+     #이 메서드가 발동이안되는데.. sender에서 비동기가 원만하게 해결이안된점, 아쉽게생각하고있습니다.    
 def on_error(ws, error):
     print(error)
 
 def on_close(ws):
-    print("### closed ###")
+    print("client closed")
 
 def on_open(ws):
-    def run(*args):
-        for i in range(3):
-            time.sleep(1)
-            text ='{ "name":"John", "age":"30", "city":"New York"}'
-     #       ws.send("Hello %d" % i)
-            ws.send(str(text))
-     #       ws.send(json.loads(text))
-    
-        time.sleep(1)
-        #ws.close()
-        print("thread terminating...")
-    thread.start_new_thread(run, ())
+    print('client open')
+    pool.spawn(sender,ws)
+    pool.join()
 
-
+def sender(ws):
+    while(True):
+        time.sleep(3)
+        text ='{"message":"'+str(os.getpid())+'"}'
+        ws.send(text)
+        
 if __name__ == "__main__":
     websocket.enableTrace(True)
-    clientsize = 5
-    for i in range(clientsize):
-        ws = websocket.WebSocketApp("ws://localhost:8000/chat",
-                              on_message = on_message,
+    #클라이언트 사이즈
+    clientsize = 4
+
+    procs = []
+
+    ws = websocket.WebSocketApp("ws://localhost:8000/chat",
                               on_error = on_error,
                               on_close = on_close)
-        ws.on_open = on_open
-        ws.run_forever()
+    ws.on_message = on_message
+    ws.on_open = on_open
+  
+    for i in range(clientsize):
+        proc = Process(target=ws.run_forever)
+        procs.append(proc)
+        proc.start()
+    for proc in procs:
+        proc.join()
+            
+                    
+  
